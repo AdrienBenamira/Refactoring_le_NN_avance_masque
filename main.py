20aaae5e5e7875f44d55cee842a1902b64b97a06
adriben <benamira.adrien@gmail.com>
2020-05-19 22:04:04 +0800

    fix bug cuda

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ from src.utils.initialisation_run import init_all_for_run, init_cipher
 from src.utils.config import Config
 import argparse
 from src.utils.utils import str2bool, two_args_str_int, two_args_str_float, str2list, transform_input_type
-
+from multiprocessing import set_start_method
+try:
+    set_start_method('spawn')
+except RuntimeError:
+    pass
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # initiate the parser
