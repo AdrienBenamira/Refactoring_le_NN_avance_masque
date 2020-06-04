d4a152987d8ea8ff7fc879c3102a211b8de0e04e
adriben <benamira.adrien@gmail.com>
2020-06-04 17:50:02 +0800

    run expe david

--- run_expe_create_models.py
+++ run_expe_create_models.py
@@ -4,19 +4,15 @@ import os
 #["baseline", "deepset", "cnn_attention", "multihead"]
 
 
-for curriculum in ["No", "Yes"]:
-    for model in ["baseline","cnn_attention", "multihead"]:
-        for continuous in ["No", "Yes"]:
-            for round in ([5, 6, 7, 8]):
-                command = "python3 main.py --nombre_round_eval " + str(round) + " --inputs_type '[ctdata0l^ctdata1l, ctdata0r^ctdata1r^ctdata0l^ctdata1l, ctdata0l^ctdata0r, ctdata1l^ctdata1r]' " + " --type_model " + str(model) + " --countinuous_learning " + str(continuous) + " --curriculum_learning " + str(curriculum)
-                os.system(command)
+for model in ["baseline"]:
+    for round in ([5, 6, 7, 8]):
+        command = "python3 main.py --nombre_round_eval " + str(round) + " --inputs_type '[ctdata0l^ctdata1l, ctdata0r^ctdata1r, inv(V0)&inv(V1)]' " + " --type_model " + str(model)
+        os.system(command)
 
 
-for curriculum in ["No", "Yes"]:
-    for continuous in ["No", "Yes"]:
-        for model in ["baseline"]:
-            for round in ([5, 6, 7, 8]):
-                command = "python3 main.py --nombre_round_eval " + str(round) + " --inputs_type '[ctdata0l^ctdata1l, ctdata0r^ctdata1r^ctdata0l^ctdata1l, ctdata0l^ctdata0r]' " + " --type_model " + str(model) + " --countinuous_learning " + str(continuous) + " --curriculum_learning " + str(curriculum)
-                os.system(command)
+for model in ["baseline","cnn_attention", "multihead"]:
+    for round in ([5, 6, 7, 8]):
+        command = "python3 main.py --nombre_round_eval " + str(round) + " --inputs_type '[ctdata0l^ctdata1l, inv(DeltaV), inv(V0)&inv(V1), inv(V0)&V1]' " + " --type_model " + str(model)
+        os.system(command)
 
 
