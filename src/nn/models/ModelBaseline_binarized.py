5741c889f10fd70c0fda2098dc951d2c84c6f7ce
adriben <benamira.adrien@gmail.com>
2020-06-08 14:51:42 +0800

    run expe binary 2

--- src/nn/models/ModelBaseline_binarized.py
+++ src/nn/models/ModelBaseline_binarized.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-
+import numpy as np
 
 import math
 
@@ -11,38 +11,41 @@ class ModelPaperBaseline_bin(nn.Module):
     def __init__(self, args):
         super(ModelPaperBaseline_bin, self).__init__()
         self.args = args
+        Conv1d = conv1d_Q_fn(w_bit=1)
+        Linear = linear_Q_fn(w_bit=1)
+        self.act_q = activation_quantize_fn(a_bit=2)
         self.word_size = args.word_size
-        self.conv0 = BinaryConv1d(in_channels=len(self.args.inputs_type), out_channels=args.out_channel0, kernel_size=1)
+        self.conv0 = Conv1d(in_channels=len(self.args.inputs_type), out_channels=args.out_channel0, kernel_size=1)
         self.BN0 = nn.BatchNorm1d(args.out_channel0, eps=0.01, momentum=0.99)
         self.layers_conv = nn.ModuleList()
         self.layers_batch = nn.ModuleList()
         self.numLayers = args.numLayers
         for i in range(args.numLayers - 1):
-            self.layers_conv.append(BinaryConv1d(in_channels=args.out_channel1, out_channels=args.out_channel1, kernel_size=3, padding=1))
+            self.layers_conv.append(Conv1d(in_channels=args.out_channel1, out_channels=args.out_channel1, kernel_size=3, padding=1))
             self.layers_batch.append(nn.BatchNorm1d(args.out_channel1, eps=0.01, momentum=0.99))
-        self.fc1 = BinaryLinear(args.out_channel1 * args.word_size, args.hidden1)  # 6*6 from image dimension
+        self.fc1 = Linear(args.out_channel1 * args.word_size, args.hidden1)  # 6*6 from image dimension
         self.BN5 = nn.BatchNorm1d(args.hidden1, eps=0.01, momentum=0.99)
-        self.fc2 = BinaryLinear(args.hidden1, args.hidden1)
+        self.fc2 = Linear(args.hidden1, args.hidden1)
         self.BN6 = nn.BatchNorm1d(args.hidden1, eps=0.01, momentum=0.99)
-        self.fc3 = BinaryLinear(args.hidden1, 1)
-        self.bintanh  = BinaryTanh()
+        self.fc3 = Linear(args.hidden1, 1)
+
 
 
     def forward(self, x):
         x = x.view(-1, len(self.args.inputs_type), self.word_size)
         self.x_input = x
-        x = self.bintanh(self.BN0(self.conv0(x)))
+        x = self.act_q(F.relu(self.BN0(self.conv0(x))))
         shortcut = x.clone()
         self.shorcut = shortcut[0]
         for i in range(len(self.layers_conv)):
             x = self.layers_conv[i](x)
             x = self.layers_batch[i](x)
-            x = self.bintanh(x)
+            x = self.act_q(F.relu(x))
             x = x + shortcut
         x = x.view(x.size(0), -1)
-        x = self.bintanh(self.BN5(self.fc1(x)))
+        x = self.act_q(F.relu(self.BN5(self.fc1(x))))
         self.intermediare = x.clone()
-        x = self.bintanh(self.BN6(self.fc2(x)))
+        x = self.act_q(F.relu(self.BN6(self.fc2(x))))
         x = self.fc3(x)
         x = torch.sigmoid(x)
         return x
@@ -55,77 +58,96 @@ class ModelPaperBaseline_bin(nn.Module):
             self.layers_batch[i].weight.requires_grad = False
 
 
-from torch.autograd import Function
 
-class BinarizeF(Function):
-
-    @staticmethod
-    def forward(cxt, input):
-        output = input.new(input.size())
-        output[input >= 0] = 1
-        output[input < 0] = -1
-        return output
+def uniform_quantize(k):
+  class qfn(torch.autograd.Function):
 
     @staticmethod
-    def backward(cxt, grad_output):
-        grad_input = grad_output.clone()
-        return grad_input
+    def forward(ctx, input):
+      if k == 32:
+        out = input
+      elif k == 1:
+        out = torch.sign(input)
+      else:
+        n = float(2 ** k - 1)
+        out = torch.round(input * n) / n
+      return out
 
-# aliases
-binarize = BinarizeF.apply
+    @staticmethod
+    def backward(ctx, grad_output):
+      grad_input = grad_output.clone()
+      return grad_input
+
+  return qfn().apply
 
 
+class weight_quantize_fn(nn.Module):
+  def __init__(self, w_bit):
+    super(weight_quantize_fn, self).__init__()
+    assert w_bit <= 8 or w_bit == 32
+    self.w_bit = w_bit
+    self.uniform_q = uniform_quantize(k=w_bit)
 
-class BinaryConv1d(nn.Conv1d):
+  def forward(self, x):
+    if self.w_bit == 32:
+      weight_q = x
+    elif self.w_bit == 1:
+      E = torch.mean(torch.abs(x)).detach()
+      weight_q = self.uniform_q(x / E) * E
+    else:
+      weight = torch.tanh(x)
+      max_w = torch.max(torch.abs(weight)).detach()
+      weight = weight / 2 / max_w + 0.5
+      weight_q = max_w * (2 * self.uniform_q(weight) - 1)
+    return weight_q
+
+
+class activation_quantize_fn(nn.Module):
+  def __init__(self, a_bit):
+    super(activation_quantize_fn, self).__init__()
+    assert a_bit <= 8 or a_bit == 32
+    self.a_bit = a_bit
+    self.uniform_q = uniform_quantize(k=a_bit)
+
+  def forward(self, x):
+    if self.a_bit == 32:
+      activation_q = x
+    else:
+      activation_q = self.uniform_q(torch.clamp(x, 0, 1))
+      # print(np.unique(activation_q.detach().numpy()))
+    return activation_q
+
+
+def conv1d_Q_fn(w_bit):
+  class Conv1d_Q(nn.Conv1d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
+      super(Conv1d_Q, self).__init__(in_channels, out_channels, kernel_size, stride,
+                                     padding, dilation, groups, bias)
+      self.w_bit = w_bit
+      self.quantize_fn = weight_quantize_fn(w_bit=w_bit)
+
+    def forward(self, input, order=None):
+      weight_q = self.quantize_fn(self.weight)
+      # print(np.unique(weight_q.detach().numpy()))
+      return F.conv1d(input, weight_q, self.bias, self.stride,
+                      self.padding, self.dilation, self.groups)
+
+  return Conv1d_Q
+
+
+def linear_Q_fn(w_bit):
+  class Linear_Q(nn.Linear):
+    def __init__(self, in_features, out_features, bias=True):
+      super(Linear_Q, self).__init__(in_features, out_features, bias)
+      self.w_bit = w_bit
+      self.quantize_fn = weight_quantize_fn(w_bit=w_bit)
 
     def forward(self, input):
-        bw = binarize(self.weight)
-        return F.conv1d(input, bw, self.bias, self.stride,
-                               self.padding, self.dilation, self.groups)
+      weight_q = self.quantize_fn(self.weight)
+      # print(np.unique(weight_q.detach().numpy()))
+      return F.linear(input, weight_q, self.bias)
 
-    def reset_parameters(self):
-        # Glorot initialization
-        in_features = self.in_channels
-        out_features = self.out_channels
-        for k in self.kernel_size:
-            in_features *= k
-            out_features *= k
-        stdv = math.sqrt(1.5 / (in_features + out_features))
-        self.weight.data.uniform_(-stdv, stdv)
-        if self.bias is not None:
-            self.bias.data.zero_()
-        self.weight.lr_scale = 1. / stdv
+  return Linear_Q
 
-
-class BinaryTanh(nn.Module):
-    def __init__(self):
-        super(BinaryTanh, self).__init__()
-        self.hardtanh = nn.Hardtanh()
-
-    def forward(self, input):
-        output = self.hardtanh(input)
-        output = binarize(output)
-        return output
-
-
-
-
-class BinaryLinear(nn.Linear):
-
-    def forward(self, input):
-        binary_weight = binarize(self.weight)
-        if self.bias is None:
-            return F.linear(input, binary_weight)
-        else:
-            return F.linear(input, binary_weight, self.bias)
-
-    def reset_parameters(self):
-        # Glorot initialization
-        in_features, out_features = self.weight.size()
-        stdv = math.sqrt(1.5 / (in_features + out_features))
-        self.weight.data.uniform_(-stdv, stdv)
-        if self.bias is not None:
-            self.bias.data.zero_()
-
-        self.weight.lr_scale = 1. / stdv
 
