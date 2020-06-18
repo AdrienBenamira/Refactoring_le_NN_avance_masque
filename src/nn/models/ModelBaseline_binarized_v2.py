c8c4ce2975c7bb8c7354f51a08fdb52c7957e86d
adriben <benamira.adrien@gmail.com>
2020-06-19 02:10:21 +0800

    run expe 0-1 v3 kernel size 3 v2

--- src/nn/models/ModelBaseline_binarized_v2.py
+++ src/nn/models/ModelBaseline_binarized_v2.py
@@ -26,7 +26,7 @@ class ModelPaperBaseline_bin2(nn.Module):
                 self.layers_batch.append(nn.BatchNorm1d(args.out_channel1, eps=0.01, momentum=0.99))
             else:
                 self.layers_conv.append(
-                nn.Conv1d(in_channels=args.out_channel1, out_channels=args.out_channel1, kernel_size=3, padding =1))
+                nn.Conv1d(in_channels=args.out_channel1, out_channels=args.out_channel1, kernel_size=1))
                 self.layers_batch.append(nn.BatchNorm1d(args.out_channel1, eps=0.01, momentum=0.99))
         self.fc1 = nn.Linear(args.out_channel1 * args.word_size, args.hidden1)  # 6*6 from image dimension
         self.BN5 = nn.BatchNorm1d(args.hidden1, eps=0.01, momentum=0.99)
