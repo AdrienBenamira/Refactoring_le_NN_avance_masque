cbaf4774c764925537ba95e3bb372309ba1706ba
adriben <benamira.adrien@gmail.com>
2020-07-03 17:09:37 +0800

    run v2 + train linear 1024 -> 512 (embedding) -> 0/1

--- src/nn/models/Model_linear.py
+++ src/nn/models/Model_linear.py
@@ -11,19 +11,19 @@ class NN_linear(nn.Module):
 
     def __init__(self, args, input_shape):
         super(NN_linear, self).__init__()
-        self.embedding = 1024
+        self.embedding = 16
         self.args = args
         self.word_size = args.word_size
         self.act_q = activation_quantize_fn(a_bit=1)
-        self.fc1 = nn.Linear(input_shape,  self.embedding)  # 6*6 from image dimension
-        self.BN5 = nn.BatchNorm1d(self.embedding, eps=0.01, momentum=0.99)
-        #self.fc2 = nn.Linear(args.hidden1,self.embedding)
-        #self.BN6 = nn.BatchNorm1d(self.embedding, eps=0.01, momentum=0.99)
+        self.fc1 = nn.Linear(input_shape,  1024)  # 6*6 from image dimension
+        self.BN5 = nn.BatchNorm1d(1024, eps=0.01, momentum=0.99)
+        self.fc2 = nn.Linear(1024,self.embedding)
+        self.BN6 = nn.BatchNorm1d(self.embedding, eps=0.01, momentum=0.99)
         self.fc3 = nn.Linear(self.embedding, 1)
 
     def forward(self, x):
         x = F.relu(self.BN5(self.fc1(x)))
-        #x = F.relu(self.BN6(self.fc2(x)))
+        x = F.relu(self.BN6(self.fc2(x)))
         x = self.act_q(x)
         x = self.fc3(x)
         x = torch.sigmoid(x)
